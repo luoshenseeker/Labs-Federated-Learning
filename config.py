77e214f334d7224273c7a83bec73a1077d11f537
luoshenseeker <635598002@qq.com>
2022-04-15 12:50:32 +0800

    config update

--- config.py
+++ config.py
@@ -1,11 +1,22 @@
-USE_GPU = True
+import platform
 
 # dataset name
 DATASET_NAME = 'MNIST_iid'  # Unbalanced, alpha=10.0, Balanced→bbal
 # assert DATASET_NAME in ['ml-1m', 'pinterest-20']
 
-# paths
-ROOT_PATH = '/home/shengy/luoshenseeker/Labs-Federated-Learning/'
+sysstr = platform.system()
+if sysstr == "Linux":
+    USE_GPU = True
+    # paths
+    ROOT_PATH = '/home/shengy/luoshenseeker/Labs-Federated-Learning/'
+elif sysstr == "Windows":
+    USE_GPU = False
+    # paths
+    ROOT_PATH = "C:\\Users\\luoshenseeker\\home\\work\\科研\\Selection\\Labs-Federated-Learning\\"
+else:
+    USE_GPU = True
+    # paths
+    ROOT_PATH = '/home/shengy/luoshenseeker/Labs-Federated-Learning/'
 
 # train_rating = main_path + '{}.train.rating'.format(dataset)
 # test_rating = main_path + '{}.test.rating'.format(dataset)
