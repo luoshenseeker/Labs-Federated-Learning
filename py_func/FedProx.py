a0ea9db456e0915024bd5ca915be871bee6887a9
luoshenseeker <635598002@qq.com>
2022-04-20 00:52:52 +0800

    create clustered

--- py_func/FedProx.py
+++ py_func/FedProx.py
@@ -1349,3 +1349,217 @@ def SCAFFOLD_FedAvg_sampling(
     )
 
     return model, loss_hist, acc_hist
+
+def SCAFFOLD_clustered_sampling(
+    sampling: str,
+    model,
+    n_sampled: int,
+    training_sets: list,
+    testing_sets: list,
+    training_sets_full,
+    testing_sets_full,
+    n_iter: int,
+    n_SGD: int,
+    lr: float,
+    file_name: str,
+    batch_size: int,
+    sim_type: str,
+    iter_FP=0,
+    decay=1.0,
+    metric_period=1,
+    mu=0.0,
+):
+    """all the clients are considered in this implementation of FedProx
+    Parameters:
+        - `model`: common structure used by the clients and the server
+        - `training_sets`: list of the training sets. At each index is the
+            trainign set of client "index"
+        - `n_iter`: number of iterations the server will run
+        - `testing_set`: list of the testing sets. If [], then the testing
+            accuracy is not computed
+        - `mu`: regularixation term for FedProx. mu=0 for FedAvg
+        - `epochs`: number of epochs each client is running
+        - `lr`: learning rate of the optimizer
+        - `decay`: to change the learning rate at each iteration
+
+    returns :
+        - `model`: the final global model
+    """
+
+    from scipy.cluster.hierarchy import linkage
+    from py_func.clustering import get_matrix_similarity_from_grads
+
+    if sampling == "clustered_2":
+        from py_func.clustering import get_clusters_with_alg2
+    from py_func.clustering import sample_clients
+
+    loss_f = loss_classifier
+
+    # Variables initialization
+    K = len(training_sets)  # number of clients
+    controls_group = [deepcopy(server_controls) for i in range(K)]             # init clients control and related vars needed later
+    # server_controls = [deepcopy(server_controls) for i in range(K)]
+    delta_controls_group = [deepcopy(server_controls) for i in range(K)]
+    delta_model_group = [deepcopy(server_controls) for i in range(K)]
+
+    n_samples = np.array([len(db.dataset) for db in training_sets])
+    weights = n_samples / np.sum(n_samples)
+    print("Clients' weights:", weights)
+
+    loss_hist = np.zeros((n_iter + 1, K))
+    acc_hist = np.zeros((n_iter + 1, K))
+
+    for k, dl in enumerate(training_sets):
+        loss_hist[0, k] = float(loss_dataset(model, dl, loss_f).detach())
+        acc_hist[0, k] = accuracy_dataset(model, dl)
+
+    # LOSS AND ACCURACY OF THE INITIAL MODEL
+    server_loss = np.dot(weights, loss_hist[0])
+    server_acc = np.dot(weights, acc_hist[0])
+    print(f"====> i {sampling}: 0 Loss: {server_loss} Test Accuracy: {server_acc}")
+
+    sampled_clients_hist = np.zeros((n_iter, K)).astype(int)
+
+    # INITILIZATION OF THE GRADIENT HISTORY AS A LIST OF 0
+
+    if sampling == "clustered_1":
+        from py_func.clustering import get_clusters_with_alg1
+
+        distri_clusters = get_clusters_with_alg1(n_sampled, weights)
+
+    elif sampling == "clustered_2":
+        from py_func.clustering import get_gradients
+
+        gradients = get_gradients(sampling, model, [model] * K)
+
+    for i in range(n_iter):
+
+        clients_params = []
+        clients_params_delta_control = []
+        clients_params_delta_model = []
+        total_samples = 0
+
+        previous_global_model = deepcopy(model)
+
+        clients_params = []
+        clients_models = []
+        sampled_clients_for_grad = []
+
+        if i < iter_FP:
+            print("MD sampling, now quit!")
+        #
+        #     np.random.seed(i)
+        #     sampled_clients = np.random.choice(
+        #         K, size=n_sampled, replace=True, p=weights
+        #     )
+        #
+        #     for k in sampled_clients:
+        #
+        #         local_model = deepcopy(model)
+        #         local_optimizer = optim.SGD(local_model.parameters(), lr=lr)
+        #
+        #         local_learning(
+        #             local_model,
+        #             mu,
+        #             local_optimizer,
+        #             training_sets[k],
+        #             n_SGD,
+        #             loss_f,
+        #         )
+        #
+        #         # SAVE THE LOCAL MODEL TRAINED
+        #         list_params = list(local_model.parameters())
+        #         list_params = [
+        #             tens_param.detach() for tens_param in list_params
+        #         ]
+        #         clients_params.append(list_params)
+        #         clients_models.append(deepcopy(local_model))
+        #
+        #         sampled_clients_for_grad.append(k)
+        #         sampled_clients_hist[i, k] = 1
+
+        else:
+            if sampling == "clustered_2":
+
+                # GET THE CLIENTS' SIMILARITY MATRIX
+                sim_matrix = get_matrix_similarity_from_grads(
+                    gradients, distance_type=sim_type
+                )
+
+                # GET THE DENDROGRAM TREE ASSOCIATED
+                linkage_matrix = linkage(sim_matrix, "ward")
+
+                distri_clusters = get_clusters_with_alg2(
+                    linkage_matrix, n_sampled, weights
+                )
+
+            for k in sample_clients(distri_clusters):
+
+                local_model = deepcopy(model)
+                local_optimizer = optim.SGD(local_model.parameters(), lr=lr)
+
+                local_learning(
+                    local_model,
+                    mu,
+                    local_optimizer,
+                    training_sets[k],
+                    n_SGD,
+                    loss_f,
+                )
+
+                # SAVE THE LOCAL MODEL TRAINED
+                list_params = list(local_model.parameters())
+                list_params = [
+                    tens_param.detach() for tens_param in list_params
+                ]
+                clients_params.append(list_params)
+                clients_models.append(deepcopy(local_model))
+
+                sampled_clients_for_grad.append(k)
+                sampled_clients_hist[i, k] = 1
+
+        # CREATE THE NEW GLOBAL MODEL AND SAVE IT
+        model = FedAvg_agregation_process(
+            deepcopy(model), clients_params, weights=[1 / n_sampled] * n_sampled
+        )
+
+        # COMPUTE THE LOSS/ACCURACY OF THE DIFFERENT CLIENTS WITH THE NEW MODEL
+        if i % metric_period == 0:
+
+            for k, dl in enumerate(training_sets):
+                loss_hist[i + 1, k] = float(
+                    loss_dataset(model, dl, loss_f).detach()
+                )
+
+            for k, dl in enumerate(testing_sets):
+                acc_hist[i + 1, k] = accuracy_dataset(model, dl)
+
+            server_loss = np.dot(weights, loss_hist[i + 1])
+            server_acc = np.dot(weights, acc_hist[i + 1])
+
+            print(
+                f"====> i {sampling}: {i+1} Loss: {server_loss} Server Test Accuracy: {server_acc}"
+            )
+
+        # UPDATE THE HISTORY OF LATEST GRADIENT
+        if sampling == "clustered_2":
+            gradients_i = get_gradients(
+                sampling, previous_global_model, clients_models
+            )
+            for idx, gradient in zip(sampled_clients_for_grad, gradients_i):
+                gradients[idx] = gradient
+
+        lr *= decay
+
+    # SAVE THE DIFFERENT TRAINING HISTORY
+    #    save_pkl(models_hist, "local_model_history", file_name)
+    #    save_pkl(server_hist, "server_history", file_name)
+    save_pkl(loss_hist, "loss", file_name)
+    save_pkl(acc_hist, "acc", file_name)
+    save_pkl(sampled_clients_hist, "sampled_clients", file_name)
+
+    torch.save(
+        model.state_dict(), f"saved_exp_info/final_model/{file_name}.pth"
+    )
+
+    return model, loss_hist, acc_hist
