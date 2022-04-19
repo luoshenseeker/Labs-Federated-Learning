7dca33f77ce0b0945c1d33a6f9ba43ad4d4d193e
luoshenseeker <635598002@qq.com>
2022-04-20 01:01:41 +0800

    stratified creation

--- py_func/FedProx.py
+++ py_func/FedProx.py
@@ -1554,3 +1554,182 @@ def SCAFFOLD_clustered_sampling(
     )
 
     return model, loss_hist, acc_hist
+
+def SCAFFOLD_stratified_sampling(
+    dataset: str,
+    sampling: str,
+    model,
+    n_sampled: int,
+    training_sets: list,
+    testing_sets: list,
+    n_iter: int,
+    n_SGD: int,
+    lr: float,
+    file_name: str,
+    sim_type: str,
+    iter_FP=0,
+    decay=1.0,
+    metric_period=1,
+    mu=0.0,
+):
+    """all the clients are considered in this implementation of FedProx
+        Parameters:
+            - `model`: common structure used by the clients and the server
+            - `training_sets`: list of the training sets. At each index is the
+                trainign set of client "index"
+            - `n_iter`: number of iterations the server will run
+            - `testing_set`: list of the testing sets. If [], then the testing
+                accuracy is not computed
+            - `mu`: regularixation term for FedProx. mu=0 for FedAvg
+            - `epochs`: number of epochs each client is running
+            - `lr`: learning rate of the optimizer
+            - `decay`: to change the learning rate at each iteration
+
+        returns :
+            - `model`: the final global model
+        """
+
+    from py_func.clustering import get_matrix_similarity_from_grads
+
+    loss_f = loss_classifier
+
+    # Variables initialization
+    K = len(training_sets)  # number of clients
+    n_samples = np.array([len(db.dataset) for db in training_sets])
+    weights = n_samples / np.sum(n_samples)
+    print("Clients' weights:", weights)
+
+    # if dataset[:5] == "MNIST":
+    #     list_dls_train, list_dls_test, list_dls_train_full, list_dls_test_full = get_MNIST_dataloaders(dataset, batch_size)
+    # elif dataset[:5] == "CIFAR":
+    #     fr = open('saved_exp_info/cluster_result/cifar10_balance_dir.pkl', 'rb')
+    #     cluster_result = pickle.load(fr)
+    # elif dataset[:6] == "FMNIST":
+    #     list_dls_train, list_dls_test, list_dls_train_full, list_dls_test_full = get_FMNIST_dataloaders(dataset, batch_size)
+
+    from cluster.cluster_train import cluster_training
+
+    result_path = cluster_training(dataset)
+    fr = open(result_path, 'rb')
+    cluster_result = pickle.load(fr)
+
+    N_CLASSES = len(cluster_result)
+    SIZE_CLASSES = [len(cls) for cls in cluster_result]
+    N_CLIENTS = sum(len(c) for c in cluster_result)  # number of clients
+
+    loss_hist = np.zeros((n_iter + 1, K))
+    acc_hist = np.zeros((n_iter + 1, K))
+
+    for k, dl in enumerate(training_sets):
+        loss_hist[0, k] = float(loss_dataset(model, dl, loss_f).detach())
+        acc_hist[0, k] = accuracy_dataset(model, dl)
+
+    # LOSS AND ACCURACY OF THE INITIAL MODEL
+    server_loss = np.dot(weights, loss_hist[0])
+    server_acc = np.dot(weights, acc_hist[0])
+    print(f"====> i {sampling}: 0 Loss: {server_loss} Test Accuracy: {server_acc}")
+
+    sampled_clients_hist = np.zeros((n_iter, K)).astype(int)
+
+    # INITILIZATION OF THE GRADIENT HISTORY AS A LIST OF 0
+
+    for i in range(n_iter):
+
+        clients_params = []
+        clients_models = []
+        sampled_clients_for_grad = []
+
+        # 鲁棒性问题！抽样率p的改变
+        # GET THE CLIENTS' CHOSEN PROBABILITY
+        chosen_p = np.zeros((N_CLASSES, N_CLIENTS)).astype(float)
+        for j, cls in enumerate(cluster_result):
+            for k in range(N_CLIENTS):
+                if k in cls:
+                    chosen_p[j][k] = round(1/SIZE_CLASSES[j], 12)
+
+        # chosen_p /= float(chosen_p.sum(), 12)  # normalize
+
+        # np.set_printoptions(threshold=np.inf)  # 解决显示不完全问题
+        # print(chosen_p)
+        # print(np.shape(chosen_p))
+
+        # distri_clusters = np.zeros((n_sampled, n_clients))
+        # for l in range(n_sampled):
+        #     distri_clusters[l] /= np.sum(distri_clusters[l])
+
+        from py_func.clustering import sample_clients
+
+        # allocation需更改每一类采的个数相同问题
+        # shard分片分10类，从每类中取一个
+        # print("手动实现随机抽样！！！")
+        # from numpy.random import choice
+        # selects = choice(100, 10, replace=False, p=[0.01 for _ in range(100)])
+        # for k in selects:
+
+        print("### chosen client index: ###")
+        for _ in range(int(n_sampled / N_CLASSES)):  # 每个class抽两个client训练，每个epoch中12345,12345
+            for k in sample_clients(chosen_p):
+                print(f"{k},", end="")
+
+                local_model = deepcopy(model)
+                local_optimizer = optim.SGD(local_model.parameters(), lr=lr)
+
+                local_learning(
+                    local_model,
+                    mu,
+                    local_optimizer,
+                    training_sets[k],
+                    n_SGD,
+                    loss_f,
+                )
+
+                # SAVE THE LOCAL MODEL TRAINED
+                list_params = list(local_model.parameters())
+                list_params = [
+                    tens_param.detach() for tens_param in list_params
+                ]
+                clients_params.append(list_params)
+                clients_models.append(deepcopy(local_model))
+
+                sampled_clients_for_grad.append(k)
+                sampled_clients_hist[i, k] = 1
+        print("\b ")
+
+        # CREATE THE NEW GLOBAL MODEL AND SAVE IT
+        model = FedAvg_agregation_process(
+            deepcopy(model), clients_params, weights=[1 / n_sampled] * n_sampled
+        )
+
+        # COMPUTE THE LOSS/ACCURACY OF THE DIFFERENT CLIENTS WITH THE NEW MODEL
+
+        if i % metric_period == 0:
+
+            for k, dl in enumerate(training_sets):
+                loss_hist[i + 1, k] = float(
+                    loss_dataset(model, dl, loss_f).detach()
+                )
+
+            for k, dl in enumerate(testing_sets):
+                acc_hist[i + 1, k] = accuracy_dataset(model, dl)
+
+            server_loss = np.dot(weights, loss_hist[i + 1])
+            server_acc = np.dot(weights, acc_hist[i + 1])
+
+            print(
+                f"====> i {sampling}: {i + 1} Loss: {server_loss} Server Test Accuracy: {server_acc}"
+            )
+
+        lr *= decay
+
+    # SAVE THE DIFFERENT TRAINING HISTORY
+    # save_pkl(models_hist, "local_model_history", file_name)
+    # save_pkl(server_hist, "server_history", file_name)
+    save_pkl(loss_hist, "loss", file_name)
+    save_pkl(acc_hist, "acc", file_name)
+    save_pkl(sampled_clients_hist, "sampled_clients", file_name)
+
+    torch.save(
+        model.state_dict(), f"saved_exp_info/final_model/{file_name}.pth"
+    )
+
+    return model, loss_hist, acc_hist
